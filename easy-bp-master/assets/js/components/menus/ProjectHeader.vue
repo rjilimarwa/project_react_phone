784c03f4838b2e475afb4a0d4ac89250dcc61e9f
rjilimarwadjerba@gmail.com <admin@exemple.com>
2020-02-29 14:43:07 +0000

    a project_rect_mobile

---
+++ easy-bp-master/assets/js/components/menus/ProjectHeader.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="folder-navbar">
+        <div class="container">
+            <ul class="nav navbar-nav">
+                <li :class="{active: checkRoute('informations')}">
+                    <a :href="'folders/' + folder + '/myproject/project/informations'">Informations</a>
+                </li>
+                <li :class="{active: checkRoute('leader')}">
+                    <a :href="'folders/' + folder + '/myproject/project/leader'">Responsable</a>
+                </li>
+                <li :class="{active: checkRoute('period')}">
+                    <a :href="'folders/' + folder + '/myproject/project/period'">Période</a>
+                </li>
+                <li :class="{active: checkRoute('contact')}">
+                    <a :href="'folders/' + folder + '/myproject/project/contact'">Coordonnées</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ProjectHeader",
+        props: ['folder'],
+        data() {
+            return {
+                urlRoute: window.location.pathname.split('/').slice(1)
+            }
+        },
+        mounted() {
+            this.urlRoute.splice(1, 1);
+            this.urlRoute = this.urlRoute.join('.');
+        },
+        methods: {
+            checkRoute(value) {
+                let url = this.urlRoute.toString();
+                return url.substr(value.length * -1) === value;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ test/src/assets/js/pages/Connexion/registrer.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container first pt">
+        <div class="row">
+            <form name="login_form" @submit.prevent="withCredentials">
+                <div class="panel panel-default center-block" id="registrer">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Registre</h3>
+                    </div>
+
+                    <div class="panel-body">
+                        <div class="form-group">
+                            <div id="ic" style="display:none;" class="alert alert-danger" role="alert">
+                                <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                                <span class="sr-only">Error:</span>
+                                Indentifiants incorrects
+                            </div>
+                            <input type="email" name="email" v-model="model.email" required placeholder="Email"
+                                   autofocus class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="password" v-model="model.password" required placeholder="Mot de passe"
+                                   class="form-control">
+                        </div>
+                    </div>
+
+                    <div class="panel-footer">
+                        <a :href="'register'">S'inscrire</a>
+                        <button type="submit" class="btn btn-primary pull-right">Se connecter</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
